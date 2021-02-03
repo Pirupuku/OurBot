4af436d5d0aa29ac7f449b90c6d44f046d5ecce4
Pirupuku <55432521+Pirupuku@users.noreply.github.com>
2021-02-03 17:17:32 +0100

    Delete this_time.js

--- commands/this_time.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'this_time',
-    description: "fuck Assiss",
-    execute(message, args){
-        message.channel.send('This time the item will go to that bitch!')
-    }
-}
