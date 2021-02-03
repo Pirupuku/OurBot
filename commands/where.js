9c3e534d7ad83efa3d20e5f1cd13d3ccaa8fbb39
Pirupuku <55432521+Pirupuku@users.noreply.github.com>
2021-02-03 20:53:45 +0100

    Update where.js

---
+++ commands/where.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'where',
+    description: "where the fuck are you all!",
+    execute(message, args){
+        message.channel.send('https://imgur.com/gallery/mKKAr')
+    }
+}
