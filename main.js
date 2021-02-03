c79acf5dddbf7a23778ba2aa07179198d9c21b35
Pirupuku <55432521+Pirupuku@users.noreply.github.com>
2021-02-03 22:28:20 +0100

    Update main.js

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ bot.on('ready', () => {
 
 bot.on('message', message =>{
      if(!message.content.startsWith(prefix) || message.author.bot) return;
-     if (message..content.startsWith(prefix)) msg.delete();
+     if (message.content.startsWith(prefix)) msg.delete();
         else return;
      const args = message.content.slice(prefix.length).split(/ +/);
      const command = args.shift().toLowerCase();
