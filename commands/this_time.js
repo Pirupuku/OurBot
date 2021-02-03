fbfe48333353560b54954526f8a877666408aa7e
Pirupuku <55432521+Pirupuku@users.noreply.github.com>
2021-02-03 17:14:42 +0100

    Create this_time.js

---
+++ commands/this_time.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'this_time',
+    description: "fuck Assiss",
+    execute(message, args){
+        message.channel.send('This time the item will go to that bitch!')
+    }
+}
