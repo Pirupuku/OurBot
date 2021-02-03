c89d233407595134887c87d9740f6b98258652d5
Pirupuku <55432521+Pirupuku@users.noreply.github.com>
2021-02-03 11:41:37 +0100

    Create GuildBankLogger.lua

---
+++ GuildBankLogger.lua
@@ -0,0 +1,2 @@
+
+log = "4~Major Mana Potion*1~Jin'do's Hexxer*1~Hearthstone*1~Belt of Transcendence*19~Demonic Rune*5~Major Mana Potion*18~Nightfin Soup*1~Brilliant Wizard Oil*1~Brilliant Wizard Oil*1~Brilliant Wizard Oil*4~Flask of Distilled Wisdom*20~Demonic Rune*5~Major Mana Potion*1~Black Qiraji Resonating Crystal*"
