cd98c962af6c67a8caf2db4dca4a6bf954d5b153
ave_satanas <33603067+AveDominiInferni@users.noreply.github.com>
2021-02-03 04:53:52 +0100

    Update GuildBankLogger.lua

--- addon/GuildBankLogger.lua
+++ addon/GuildBankLogger.lua
@@ -3,21 +3,24 @@ SlashCmdList["SLASH_GBL"] = function(flag) end
 SLASH_GBL1 = "/gblog"
 function SlashCmdList.GBL(args)
 	log = ""
-	for bag = 0,4 do
-		for slot = 1,GetContainerNumSlots(bag) do
+	--Bags
+	for bag = -1, 10 do
+		local slots = GetContainerNumSlots(bag)
+		for slot = 1, slots do
 			local itemLink = GetContainerItemLink(bag,slot)
 			if itemLink then
 				local icon, itemCount = GetContainerItemInfo(bag, slot)
 				local itemName = GetItemName(itemLink)
-
 				log = table.concat({log, itemCount, " ", itemName, ","})
 			end
 		end
 	end
 end
+	
 
 function GetItemName(some_string)
 	local name_start = string.find(some_string, "%[") + 1
 	local name_end = string.find(some_string, "%]") - 1
 	return string.sub(some_string, name_start, name_end)
 end
+
