ee5f5443e178a01746427476ec4b00b3fb96e76a
Pirupuku <55432521+Pirupuku@users.noreply.github.com>
2021-02-04 21:58:25 +0100

    Update dps.js

--- commands/dps.js
+++ commands/dps.js
@@ -2,7 +2,12 @@ module.exports = {
     name: 'dps',
     description: "give discord role of DPS",
     execute(message, args){
-        message.author.send('You are a DPS now!');
-        message.member.roles.add('798507075353182218').catch(console.error);
+        
+        if (message.member.roles.has('798507075353182218')){
+            message.author.send('You already are a DPS!')
+        } else {
+            message.author.send('You are a DPS now!');
+            message.member.roles.add('798507075353182218').catch(console.error);
+        }  
     }
 }
