d17c06a69ef8a5da006e47e8fe0f7559cc018f94
Pirupuku <55432521+Pirupuku@users.noreply.github.com>
2021-02-05 08:42:29 +0100

    Create nohealer.js

---
+++ commands/nohealer.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: 'nohealer',
+    description: "remove discord role of HEALER",
+    execute(message, args){
+        
+        if (!message.member.roles.cache.has('798507050752933909')){
+            message.author.send('You are not a HEALER!');
+        } else {
+            message.member.roles.add('798507050752933909').catch(console.error);
+            message.author.send('You are not a HEALER anymore!');
+        }  
+    }
+}
