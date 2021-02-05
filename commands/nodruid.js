7e63406ef7ee7330d7659b2a7877b7041d03beae
Pirupuku <55432521+Pirupuku@users.noreply.github.com>
2021-02-05 08:44:39 +0100

    Update nodruid.js

--- commands/nodruid.js
+++ commands/nodruid.js
@@ -3,7 +3,7 @@ module.exports = {
     description: "remove discord role of DRUID",
     execute(message, args){
 
-        if (message.member.roles.cache.has('798497796541513759')){
+        if (!message.member.roles.cache.has('798497796541513759')){
             message.author.send('You are not a DRUID!');
         } else {
             message.member.roles.delete('798497796541513759').catch(console.error);
