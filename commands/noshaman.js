f4d813ed8f82f19e4de4130e1f22f8ce7f9b71f6
Pirupuku <55432521+Pirupuku@users.noreply.github.com>
2021-02-05 09:06:11 +0100

    Update noshaman.js

--- commands/noshaman.js
+++ commands/noshaman.js
@@ -6,7 +6,7 @@ module.exports = {
         if (!message.member.roles.cache.has('798497547642863626')){
             message.author.send('You are not a SHAMAN!');
         } else {
-            message.member.roles.delete('798497547642863626').catch(console.error);
+            message.member.roles.remove('798497547642863626').catch(console.error);
             message.author.send('You are not a SHAMAN anymore!');
         }
     }
