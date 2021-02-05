8c71542bb663ecf9ab586d5b41281c37e4038aca
Pirupuku <55432521+Pirupuku@users.noreply.github.com>
2021-02-05 08:37:41 +0100

    Update mage.js

--- commands/mage.js
+++ commands/mage.js
@@ -4,10 +4,10 @@ module.exports = {
     execute(message, args){
         
         if (message.member.roles.cache.has('798497704976056350')){
-            message.author.send('You already are a MAGE!')
+            message.author.send('You already are a MAGE!');
         } else {
-            message.author.send('You are a MAGE now!');
             message.member.roles.add('798497704976056350').catch(console.error);
+            message.author.send('You are a MAGE now!');
         }
     }
 }
