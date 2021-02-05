84ccfbadbca4578e19711fcb90b6fa248001e1c2
Pirupuku <55432521+Pirupuku@users.noreply.github.com>
2021-02-05 09:05:34 +0100

    Update nopaladin.js

--- commands/nopaladin.js
+++ commands/nopaladin.js
@@ -6,7 +6,7 @@ module.exports = {
         if (!message.member.roles.cache.has('799233980570402816')){
             message.author.send('You are not a PALADIN!');
         } else {
-            message.member.roles.delete('799233980570402816').catch(console.error);
+            message.member.roles.remove('799233980570402816').catch(console.error);
             message.author.send('You are not a PALADIN anymore!');
         }
     }
