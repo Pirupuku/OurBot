54007bbbd666dc661c6fa9d060fc9c5f34243a2d
Pirupuku <55432521+Pirupuku@users.noreply.github.com>
2021-02-05 07:59:27 +0100

    Update member-counter.js

--- counters/member-counter.js
+++ counters/member-counter.js
@@ -1,9 +1,9 @@
 module.exports = async(bot) =>{
-    const guild = bot.guilds.cache.get('773542499049668608').members.cache.filter(member => !member.user.bot).size;
+    const guild = bot.guilds.cache.get('773542499049668608');
     setInterval(() =>{
-        const memberCounter = guild.memberCounter;
+        const memberCounter = guild.members.cache.filter(member => !member.user.bot).size;
         const channel = guild.channels.cache.get('805473191177748491');
         channel.setName(`Total Members: ${memberCounter}`);
         console.log('Updating Member Count');
-    }, 900000);
+    }, 5000);
 }
