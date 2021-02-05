3289c75f9be7f7c37f0f54fc2b15dafd957a1fac
Pirupuku <55432521+Pirupuku@users.noreply.github.com>
2021-02-05 08:46:43 +0100

    Create nopaladin.js

---
+++ commands/nopaladin.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: 'nopaladin',
+    description: "remove discord role of PALADIN",
+    execute(message, args){
+        
+        if (!message.member.roles.cache.has('799233980570402816')){
+            message.author.send('You are not a PALADIN!');
+        } else {
+            message.member.roles.delete('799233980570402816').catch(console.error);
+            message.author.send('You are not a PALADIN anymore!');
+        }
+    }
+}
