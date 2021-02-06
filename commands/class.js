9bd3ff35ae96908744980d0069efba0b7d8e04f8
Pirupuku <55432521+Pirupuku@users.noreply.github.com>
2021-02-06 11:07:41 +0100

    more commands!!!

--- commands/class.js
+++ commands/class.js
@@ -14,137 +14,201 @@ module.exports = {
         const classMage = '798497704976056350';
 
         if (args[0] == 'druid'){
-            if (message.member.roles.cache.has('798497796541513759')){
+            if (message.member.roles.cache.has(classDruid){
                 message.author.send('You already are a DRUID!')
             } else {
-                message.member.roles.add('798497796541513759').catch(console.error);
-                message.member.roles.remove('798497755952578560').catch(console.error);
-                message.member.roles.remove(classMage).catch(console.error);
+                message.member.roles.add(classDruid).catch(console.error);
+                message.member.roles.remove(classHunter).catch(console.error);
+                message.member.roles.remove(classWarrior).catch(console.error);
+                message.member.roles.remove(classWarlock).catch(console.error);
+                message.member.roles.remove(classShaman).catch(console.error);
                 message.member.roles.remove(classPaladin).catch(console.error);
                 message.member.roles.remove(classPriest).catch(console.error);
-                message.member.roles.remove('798497604035805184').catch(console.error);
-                message.member.roles.remove('798497547642863626').catch(console.error);
-                message.member.roles.remove('798497338561789993').catch(console.error);
-                message.member.roles.remove('784698785586348032').catch(console.error); 
+                message.member.roles.remove(classRogue).catch(console.error);
+                message.member.roles.remove(classMage).catch(console.error); 
                 message.author.send('You are a DRUID now!');
             }  
         }  else if(args[0] == 'nodruid'){
-            if (!message.member.roles.cache.has('798497796541513759')){
+            if (!message.member.roles.cache.has(classDruid)){
                 message.author.send('You are not a DRUID!');
             } else {
-                message.member.roles.remove('798497796541513759').catch(console.error);
+                message.member.roles.remove(classDruid).catch(console.error);
                 message.author.send('You are not a DRUID anymore!');
             } 
         } else if(args[0] == 'hunter'){
-            if (message.member.roles.cache.has('798497755952578560')){
+            if (message.member.roles.cache.has(classHunter){
                 message.author.send('You already are a HUNTER!');
             } else {
-                message.member.roles.add('798497755952578560').catch(console.error);
+                message.member.roles.add(classHunter).catch(console.error);
+                message.member.roles.remove(classDruid).catch(console.error);
+                message.member.roles.remove(classWarrior).catch(console.error);
+                message.member.roles.remove(classWarlock).catch(console.error);
+                message.member.roles.remove(classShaman).catch(console.error);
+                message.member.roles.remove(classPaladin).catch(console.error);
+                message.member.roles.remove(classPriest).catch(console.error);
+                message.member.roles.remove(classRogue).catch(console.error);
+                message.member.roles.remove(classMage).catch(console.error); 
                 message.author.send('You are a HUNTER now!');
             }
         } else if(args[0] == 'nohunter'){
-            if (!message.member.roles.cache.has('798497755952578560')){
+            if (!message.member.roles.cache.has(classHunter)){
                 message.author.send('You are not a HUNTER!');
             } else {
-                message.member.roles.remove('798497755952578560').catch(console.error);
+                message.member.roles.remove(classHunter).catch(console.error);
                 message.author.send('You are not a HUNTER anymore!');
             }
         } else if(args[0] == 'mage'){
-            if (message.member.roles.cache.has('798497704976056350')){
+            if (message.member.roles.cache.has(classMage)){
                 message.author.send('You already are a MAGE!');
             } else {
-                message.member.roles.add('798497704976056350').catch(console.error);
+                message.member.roles.add(classMage).catch(console.error);
+                message.member.roles.remove(classDruid).catch(console.error);
+                message.member.roles.remove(classWarrior).catch(console.error);
+                message.member.roles.remove(classWarlock).catch(console.error);
+                message.member.roles.remove(classShaman).catch(console.error);
+                message.member.roles.remove(classPaladin).catch(console.error);
+                message.member.roles.remove(classPriest).catch(console.error);
+                message.member.roles.remove(classRogue).catch(console.error);
+                message.member.roles.remove(classHunter).catch(console.error);
                 message.author.send('You are a MAGE now!');
             }
         } else if(args[0] == 'nomage'){
-            if (!message.member.roles.cache.has('798497704976056350')){
+            if (!message.member.roles.cache.has(classMage)){
                 message.author.send('You are not a MAGE!');
             } else {
-                message.member.roles.remove('798497704976056350').catch(console.error);
+                message.member.roles.remove(classMage).catch(console.error);
                 message.author.send('You are not a MAGE anymore!');
             }
         } else if(args[0] == 'paladin'){
-            if (message.member.roles.cache.has('799233980570402816')){
+            if (message.member.roles.cache.has(classPaladin)){
                 message.author.send('You already are a PALADIN!');
             } else {
-                message.member.roles.add('799233980570402816').catch(console.error);
+                message.member.roles.add(classPaladin).catch(console.error);
+                message.member.roles.remove(classDruid).catch(console.error);
+                message.member.roles.remove(classWarrior).catch(console.error);
+                message.member.roles.remove(classWarlock).catch(console.error);
+                message.member.roles.remove(classShaman).catch(console.error);
+                message.member.roles.remove(classMage).catch(console.error);
+                message.member.roles.remove(classPriest).catch(console.error);
+                message.member.roles.remove(classRogue).catch(console.error);
+                message.member.roles.remove(classHunter).catch(console.error);
                 message.author.send('You are a PALADIN now!');
             }
         } else if(args[0] == 'nopaladin'){
-            if (!message.member.roles.cache.has('799233980570402816')){
+            if (!message.member.roles.cache.has(classPaladin)){
                 message.author.send('You are not a PALADIN!');
             } else {
-                message.member.roles.remove('799233980570402816').catch(console.error);
+                message.member.roles.remove(classPaladin).catch(console.error);
                 message.author.send('You are not a PALADIN anymore!');
             }
         } else if(args[0] == 'priest'){
-            if (message.member.roles.cache.has('798497653058830339')){
+            if (message.member.roles.cache.has(classPriest)){
                 message.author.send('You already are a PRIEST!');
             } else {
-                message.member.roles.add('798497653058830339').catch(console.error);
+                message.member.roles.add(classPriest).catch(console.error);
+                message.member.roles.remove(classDruid).catch(console.error);
+                message.member.roles.remove(classWarrior).catch(console.error);
+                message.member.roles.remove(classWarlock).catch(console.error);
+                message.member.roles.remove(classShaman).catch(console.error);
+                message.member.roles.remove(classMage).catch(console.error);
+                message.member.roles.remove(classPaladin).catch(console.error);
+                message.member.roles.remove(classRogue).catch(console.error);
+                message.member.roles.remove(classHunter).catch(console.error);
                 message.author.send('You are a PRIEST now!');
             }
         } else if(args[0] == 'nopriest'){
-            if (!message.member.roles.cache.has('798497653058830339')){
+            if (!message.member.roles.cache.has(classPriest)){
                 message.author.send('You are not a PRIEST!');
             } else {
-                message.member.roles.remove('798497653058830339').catch(console.error);
+                message.member.roles.remove(classPriest).catch(console.error);
                 message.author.send('You are not a PRIEST anymore!');
             }
         } else if(args[0] == 'rogue'){
-            if (message.member.roles.cache.has('798497604035805184')){
+            if (message.member.roles.cache.has(classRogue)){
                 message.author.send('You already are a ROGUE!');
             } else {
-                message.member.roles.add('798497604035805184').catch(console.error);
+                message.member.roles.add(classRogue).catch(console.error);
+                message.member.roles.remove(classDruid).catch(console.error);
+                message.member.roles.remove(classWarrior).catch(console.error);
+                message.member.roles.remove(classWarlock).catch(console.error);
+                message.member.roles.remove(classShaman).catch(console.error);
+                message.member.roles.remove(classMage).catch(console.error);
+                message.member.roles.remove(classPaladin).catch(console.error);
+                message.member.roles.remove(classPriest).catch(console.error);
+                message.member.roles.remove(classHunter).catch(console.error);
                 message.author.send('You are a ROGUE now!');
             }
         } else if(args[0] == 'norogue'){
-            if (!message.member.roles.cache.has('798497604035805184')){
+            if (!message.member.roles.cache.has(classRogue)){
                 message.author.send('You are not a ROGUE!');
             } else {
-                message.member.roles.remove('798497604035805184').catch(console.error);
+                message.member.roles.remove(classRogue).catch(console.error);
                 message.author.send('You are not a ROGUE anymore!');
             }
         } else if(args[0] == 'shaman'){
-            if (message.member.roles.cache.has('798497547642863626')){
+            if (message.member.roles.cache.has(classShaman)){
                 message.author.send('You already are a SHAMAN!');
             } else {
-                message.member.roles.add('798497547642863626').catch(console.error);
+                message.member.roles.add(classShaman).catch(console.error);
+                message.member.roles.remove(classDruid).catch(console.error);
+                message.member.roles.remove(classWarrior).catch(console.error);
+                message.member.roles.remove(classWarlock).catch(console.error);
+                message.member.roles.remove(classRogue).catch(console.error);
+                message.member.roles.remove(classMage).catch(console.error);
+                message.member.roles.remove(classPaladin).catch(console.error);
+                message.member.roles.remove(classPriest).catch(console.error);
+                message.member.roles.remove(classHunter).catch(console.error);
                 message.author.send('You are a SHAMAN now!');
             }
         } else if(args[0] == 'noshaman'){
-            if (!message.member.roles.cache.has('798497547642863626')){
+            if (!message.member.roles.cache.has(classShaman)){
                 message.author.send('You are not a SHAMAN!');
             } else {
-                message.member.roles.remove('798497547642863626').catch(console.error);
+                message.member.roles.remove(classShaman).catch(console.error);
                 message.author.send('You are not a SHAMAN anymore!');
             }
         } else if(args[0] == 'warlock'){
-            if (message.member.roles.cache.has('798497338561789993')){
+            if (message.member.roles.cache.has(classWarlock)){
                 message.author.send('You already are a WARLOCK!');
             } else {
-                message.member.roles.add('798497338561789993').catch(console.error);
+                message.member.roles.add(classWarlock).catch(console.error);
+                message.member.roles.remove(classDruid).catch(console.error);
+                message.member.roles.remove(classWarrior).catch(console.error);
+                message.member.roles.remove(classShaman).catch(console.error);
+                message.member.roles.remove(classRogue).catch(console.error);
+                message.member.roles.remove(classMage).catch(console.error);
+                message.member.roles.remove(classPaladin).catch(console.error);
+                message.member.roles.remove(classPriest).catch(console.error);
+                message.member.roles.remove(classHunter).catch(console.error);
                 message.author.send('You are a WARLOCK now!');
             }
         } else if(args[0] == 'nowarlock'){
-            if (!message.member.roles.cache.has('798497338561789993')){
+            if (!message.member.roles.cache.has(classWarlock)){
                 message.author.send('You are not a WARLOCK!');
             } else {
-                message.member.roles.remove('798497338561789993').catch(console.error);
+                message.member.roles.remove(classWarlock).catch(console.error);
                 message.author.send('You are not a WARLOCK anymore!');
             }
         } else if(args[0] == 'warrior'){
-            if (message.member.roles.cache.has('784698785586348032')){
+            if (message.member.roles.cache.has(classWarrior)){
                 message.author.send('You already are a WARRIOR!');
             } else {
-                message.member.roles.add('784698785586348032').catch(console.error);
+                message.member.roles.add(classWarrior).catch(console.error);
+                message.member.roles.remove(classDruid).catch(console.error);
+                message.member.roles.remove(classShaman).catch(console.error);
+                message.member.roles.remove(classWarlock).catch(console.error);
+                message.member.roles.remove(classRogue).catch(console.error);
+                message.member.roles.remove(classMage).catch(console.error);
+                message.member.roles.remove(classPaladin).catch(console.error);
+                message.member.roles.remove(classPriest).catch(console.error);
+                message.member.roles.remove(classHunter).catch(console.error);
                 message.author.send('You are a WARRIOR now!');
             }
         } else if(args[0] == 'nowarrior'){
-            if (!message.member.roles.cache.has('784698785586348032')){
+            if (!message.member.roles.cache.has(classWarrior)){
                 message.author.send('You are not a WARRIOR!');
             } else {
-                message.member.roles.remove('784698785586348032').catch(console.error);
+                message.member.roles.remove(classWarrior).catch(console.error);
                 message.author.send('You are not a WARRIOR anymore!');
             }
         }
--- commands/role.js
+++ commands/role.js
@@ -3,46 +3,56 @@ module.exports = {
     description: "give discord role of a role of WoW",
     execute(message, args){
         
+        const roleTank = '798507023020589066';
+        const roleHealer = '798507050752933909';
+        const roleDps = '798507075353182218';
+
         if (args[0] == 'tank'){
-            if (message.member.roles.cache.has('798507023020589066')){
+            if (message.member.roles.cache.has(roleTank)){
                 message.author.send('You already are a TANK!');
             } else {
-                message.member.roles.add('798507023020589066').catch(console.error);
+                message.member.roles.add(roleTank).catch(console.error);
+                message.member.roles.remove(roleHealer).catch(console.error);
+                message.member.roles.remove(roleDps).catch(console.error);
                 message.author.send('You are a TANK now!');
             }
         } else if (args[0] == 'notank'){
-            if (!message.member.roles.cache.has('798507023020589066')){
+            if (!message.member.roles.cache.has(roleTank)){
                 message.author.send('You are not a TANK!');
             } else {
-                message.member.roles.remove('798507023020589066').catch(console.error);
+                message.member.roles.remove(roleTank).catch(console.error);
                 message.author.send('You are not a TANK anymore!');
             }
         } else if (args[0] == 'healer'){
-            if (message.member.roles.cache.has('798507050752933909')){
+            if (message.member.roles.cache.has(roleHealer)){
                 message.author.send('You already are a HEALER!');
             } else {
-                message.member.roles.add('798507050752933909').catch(console.error);
+                message.member.roles.add(roleHealer).catch(console.error);
+                message.member.roles.remove(roletank).catch(console.error);
+                message.member.roles.remove(roleDps).catch(console.error);
                 message.author.send('You are a HEALER now!');
             }
         } else if (args[0] == 'nohealer'){
-            if (!message.member.roles.cache.has('798507050752933909')){
+            if (!message.member.roles.cache.has(roleHealer)){
                 message.author.send('You are not a HEALER!');
             } else {
-                message.member.roles.remove('798507050752933909').catch(console.error);
+                message.member.roles.remove(roleHealer).catch(console.error);
                 message.author.send('You are not a HEALER anymore!');
             }
         } else if (args[0] == 'dps'){
-            if (message.member.roles.cache.has('798507075353182218')){
+            if (message.member.roles.cache.has(roleDps)){
                 message.author.send('You already are a DPS!');
             } else {
-                message.member.roles.add('798507075353182218').catch(console.error);
+                message.member.roles.add(roleDps).catch(console.error);
+                message.member.roles.remove(roleHealer).catch(console.error);
+                message.member.roles.remove(roletank).catch(console.error);
                 message.author.send('You are a DPS now!');
             }
         } else if (args[0] == 'nodps'){
-            if (!message.member.roles.cache.has('798507075353182218')){
+            if (!message.member.roles.cache.has(roleDps)){
                 message.author.send('You are not a DPS!');
             } else {
-                message.member.roles.remove('798507075353182218').catch(console.error);  
+                message.member.roles.remove(roleDps).catch(console.error);  
                 message.author.send('You are not a DPS anymore!');
             }
         }
