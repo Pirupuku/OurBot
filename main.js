be94bfd4a5b3c8d2ef17f3e0ba62b23737c91f81
Pirupuku <55432521+Pirupuku@users.noreply.github.com>
2021-02-06 12:08:39 +0100

    gd

--- main.js
+++ main.js
@@ -26,42 +26,73 @@ bot.on('ready', () => {
 });
 
 bot.on('message', message =>{
-     if(!message.content.startsWith(prefix) || message.author.bot) return;
-     if (message.content.startsWith(prefix)) message.delete();
-        else return;
-     const args = message.content.slice(prefix.length).split(/ +/);
-     const command = args.shift().toLowerCase();
+   if(!message.content.startsWith(prefix) || message.author.bot) return;
+   if (message.content.startsWith(prefix)) message.delete();
+      else return;
+   const args = message.content.slice(prefix.length).split(/ +/);
+   const command = args.shift().toLowerCase();
 
-     if (command == 'class'){
-        if (args[0] !== undefined) {
-            bot.commands.get('class').execute(message, args);
-        } else if (args[0] === undefined) {
-           message.author.send('Please enter the class you are playing with your main! (eg: $class rogue)');
-        }
-     } else if (command == 'role'){
-         if (args[0] !== undefined) {
-            bot.commands.get('role').execute(message, args);
-         } else if (args[0] === undefined) {
-            message.author.send('Please enter the role you are playing with your main! (eg: $role healer)');
-         }
-     } else if(command === 'ping'){
-          bot.commands.get('ping').execute(message, args);
-     } else if (command == 'fokya'){
-          bot.commands.get('fokya').execute(message, args);
-     } else if (command == 'thistime'){
-         bot.commands.get('thistime').execute(message, args);
-     } else if (command == 'where'){
-         bot.commands.get('where').execute(message, args);
-     } else if (command == 'whalecum'){
-        bot.commands.get('whalecum').execute(message, args);
-     } else if (command == 'gbl_load') {
-        Logs.loadData();
-        message.channel.send("Data has been updated");
-     } else if (command == 'gb') {
-        bot.commands.get('gb').execute(message, args);
-     } else if (commmand == 'random') {
-        bot.commands.get('random').execute(message, args);
-     }
+   if (command == 'class'){
+      if (args[0] !== undefined) {
+         bot.commands.get('class').execute(message, args);
+      } else if (args[0] === undefined) {
+         message.author.send('Please enter the class you are playing with your main! (eg: $class rogue)');
+      }
+   } else if (command == 'role'){
+      if (args[0] !== undefined) {
+         bot.commands.get('role').execute(message, args);
+      } else if (args[0] === undefined) {
+         message.author.send('Please enter the role you are playing with your main! (eg: $role healer)');
+      }
+   } else if(command === 'ping'){
+      bot.commands.get('ping').execute(message, args);
+   } else if (command == 'fokya'){
+      bot.commands.get('fokya').execute(message, args);
+   } else if (command == 'thistime'){
+      bot.commands.get('thistime').execute(message, args);
+   } else if (command == 'where'){
+      bot.commands.get('where').execute(message, args);
+   } else if (command == 'whalecum'){
+      bot.commands.get('whalecum').execute(message, args);
+   } else if (command == 'gbl_load') {
+      Logs.loadData();
+      message.channel.send("Data has been updated");
+   } else if (command == 'gb') {
+      bot.commands.get('gb').execute(message, args);
+   } else if (commmand == 'random') {
+      image(message);
+   }
 });
+
+function image(message){
+   console.log(args[0]);         
+   var options = {
+      url:"http://results.dogpile.com/serp?qc=images&q=" + args[0],
+      method: "GET",
+      headers: {
+         "Accept": "text/html",
+         "User-Agent": "Chrome"
+      }
+   };
+
+   request(options, function(error, response, responseBody) {
+      if (error) {
+         return;
+      }
+
+      $ = cheerio.load(responseBody);
+
+      var links = $(".image a.link");
+
+      var urls = new Array(linkgs.length).fill(0).map((v, i) => links.eq(i).attr("href"));
+
+      console.log(urls);
+      if (!urls.length) {
+         return;
+      }
+      
+      message.channel.send(urls[Math.floor(Math.random() * urls.length)] + " " + message.guild.member.random());
+   });
+}
 
 bot.login(token);
