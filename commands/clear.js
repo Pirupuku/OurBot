24ba7049d982548c67073daf2ca685bd88a56086
Pirupuku <55432521+Pirupuku@users.noreply.github.com>
2021-02-07 11:49:31 +0100

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -1,11 +1,35 @@
+const { del } = require("request");
+
 module.exports = {
     name: 'clear',
     description: "clear discord messages",
-    execute(message, args){
-            
-            (async () => {
-                let deleted;
-                deleted = await message.channel.bulkDelete(args[0]);
-            })();
+    run: async (client, message, args) => {
+        if (message.deletable) {
+             message.delete();
+        }
+        
+        if (!message.member.hasPermission("MANAGE_MESSAGES")) {
+            return message.reply("You don't have the permission to delete messages.").then(m => m.delete(5000));
+        }
+
+        if (isNaN(args[0]) || parseInt(args[0]) <= 0) {
+            return message.reply("That's not a number and I can't delete 0 messages.").then(m => m.delete(5000));
+        }
+        
+        if (!message.guild.me.hasPermission("MANAGE_MESSAGES")) {
+            return message.reply("I don't have the permission to delete messages.").then(m => m.delete(5000));
+        }
+
+        let deleteAmount;
+        
+        if (parseInt(args[0]) > 100) {
+            deleteAmount = 100;
+        } else {
+            deleteAmount = parseInt(args[0]);
+        }
+
+        message.channel.bulkDelete(deleteAmount, true)
+            .then(deleted => message.channel.send(`I deleted \`${deleted.size}\` messages.`))
+            .catch(err => message.reply(`Something went wrong... ${err}`));
     }
 }
