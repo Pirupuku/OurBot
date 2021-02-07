bb2cf6a6232fc034e4ea7f05df11ab26293c79a3
Pirupuku <55432521+Pirupuku@users.noreply.github.com>
2021-02-08 00:14:34 +0100

    Update main.js

--- main.js
+++ main.js
@@ -65,9 +65,9 @@ bot.on('message', message =>{
    } else if (command == 'clear') {
       bot.commands.get('clear').execute(bot, message, args);
    } else if (command == 'test') {
-      const trialStatus = bot.guilds.cache.get('798498129661263882');
-      const trialAmount = trialStatus.member.cache.size;
-      console.log(trialAmount);
+      const trialstatus = bot.guilds.cache.get('773542499049668608');
+      const trialrole = trialstatus.roles.cache.has('798498129661263882').size;
+      console.log(trialrole);
    }
 });
 
