12b8ba11fde38b2e2a0ee9e696e33b411c9fec0a
Pirupuku <55432521+Pirupuku@users.noreply.github.com>
2021-02-08 21:12:54 +0100

    Update main.js

--- main.js
+++ main.js
@@ -66,6 +66,38 @@ bot.on('guildMemberAdd', async guildMember => {
    messageEmbed.react(priestEmoji);
    messageEmbed.react(rogueEmoji);
    messageEmbed.react(mageEmoji);
+
+   bot.on('messageReactionAdd', async (reaction, user) => {
+      if (reaction.message.partial) await reaction.message.fetch();
+      if (reaction.partial) await reaction.fetch();
+      if (user.bot) return;
+      if (!reaction.message.guild) return;
+
+      if (reaction.emoji.name === '<:druid:808301207206494228>') {
+         await reaction.message.guild.members.cahche.get(user.id).roles.add(druidID);
+      }
+      if (reaction.emoji.name === '<:hunter:808302350162591784>') {
+         await reaction.message.guild.members.cahche.get(user.id).roles.add(hunterID);
+      }
+      if (reaction.emoji.name === '<:warrior:808301549712572426>') {
+         await reaction.message.guild.members.cahche.get(user.id).roles.add(warriorID);
+      }
+      if (reaction.emoji.name === '<:warlock:808301265388830730>') {
+         await reaction.message.guild.members.cahche.get(user.id).roles.add(warlockID);
+      }
+      if (reaction.emoji.name === '<:paladin:808301574761742346>') {
+         await reaction.message.guild.members.cahche.get(user.id).roles.add(paladinID);
+      }
+      if (reaction.emoji.name === '<:priest:808301254677495848>') {
+         await reaction.message.guild.members.cahche.get(user.id).roles.add(priestID);
+      }
+      if (reaction.emoji.name === '<:rogue:808302389223489557>') {
+         await reaction.message.guild.members.cahche.get(user.id).roles.add(rogueID);
+      }
+      if (reaction.emoji.name === '<:mage:798497704976056350>') {
+         await reaction.message.guild.members.cahche.get(user.id).roles.add(mageID);
+      }
+   });
 });
 
 bot.on('message', message =>{
