ef863de21af85ce14db589a15265b8b702ff9aa0
Pirupuku <55432521+Pirupuku@users.noreply.github.com>
2021-02-08 09:05:04 +0100

    a

--- counters/member-counter.js
+++ counters/member-counter.js
@@ -1,9 +1,9 @@
 module.exports = async(bot, message) =>{
-    const guild = bot.guilds.cache.get('773542499049668608');
+    const guildbot = bot.guilds.cache.get('773542499049668608');
     
     setInterval(() =>{
-        const memberCounter = guild.members.cache.filter(m => !m.user.bot).size;
-        const channel = guild.channels.cache.get('805473191177748491');
+        const memberCounter = guildbot.members.cache.filter(m => !m.user.bot).size;
+        const channel = guildbot.channels.cache.get('805473191177748491');
         channel.setName(`Total Members: ${memberCounter}`);
 
         let trialID = '798498129661263882';
