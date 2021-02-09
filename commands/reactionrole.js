da15b5a3bf03672bd3575688d95536e31204f358
Pirupuku <55432521+Pirupuku@users.noreply.github.com>
2021-02-09 15:32:55 +0100

    Create reactionrole.js

---
+++ commands/reactionrole.js
@@ -0,0 +1,11 @@
+module.exports = {
+  name: 'reactionrole',
+  description: "Sets up a reaction role message!",
+  execute(message, args, Discord, client) {
+  
+    const channel = '';
+  
+  }
+
+
+}
