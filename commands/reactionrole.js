141eaa8e2e1aef75f988a2543f71f39357b0d91c
Pirupuku <55432521+Pirupuku@users.noreply.github.com>
2021-02-09 15:50:51 +0100

    Update reactionrole.js

--- commands/reactionrole.js
+++ commands/reactionrole.js
@@ -4,7 +4,47 @@ module.exports = {
   execute(message, args, Discord, client) {
   
     const channel = '808584658765348865';
-  
+    const druidID = '798497796541513759';
+    const hunterID = '798497755952578560';
+    const warriorID = '784698785586348032';
+    const warlockID = '798497338561789993';
+    const paladinID = '799233980570402816';
+    const priestID = '798497653058830339';
+    const rogueID = '798497604035805184';
+    const mageID = '798497704976056350';
+    const tankID = '798507023020589066';
+    const healerID = '798507050752933909';
+    const dpsID = '798507075353182218';
+
+    const druidEmoji = '808301207206494228';
+    const hunterEmoji = '808302350162591784';
+    const warriorEmoji = '808301549712572426';
+    const warlockEmoji = '808301265388830730';
+    const paladinEmoji = '808301574761742346';
+    const priestEmoji = '808301254677495848';
+    const rogueEmoji = '808302389223489557';
+    const mageEmoji = '808301245877059604';
+    const tankEmoji = '808302563530768394';
+    const healerEmoji = '808302584103305236';
+    const dpsEmoji = '808302546677923860';
+    
+    const druidRole = message.guild.roles.cache.has(druidID);
+    const hunterRole = message.guild.roles.cache.has(hunterID);
+    const warriorRole = message.guild.roles.cache.has(warriorID);
+    const warlockRole = message.guild.roles.cache.has(warlockID);
+    const paladinRole = message.guild.roles.cache.has(paladinID);
+    const priestRole = message.guild.roles.cache.has(priestID);
+    const rogueRole = message.guild.roles.cache.has(rogueID);
+    const mageEole = message.guild.roles.cache.has(mageID);
+    const tankRole = message.guild.roles.cache.has(tankID);
+    const healerRole = message.guild.roles.cache.has(healerID);
+    const dpsRole = message.guild.roles.cache.has(dpsID);
+    
+    let embed = new Discord.MessageEmbed()
+      .setColor('#004a93')
+      .setTitle('Chose the class of your main character.')
+      .setDescription('Chosing a class is a must to get **Member**-status later.\n\n'
+        + 
   }
 
 
