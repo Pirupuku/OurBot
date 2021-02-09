f334c480b1fa49952b3982ca7ca994e14681d93d
Pirupuku <55432521+Pirupuku@users.noreply.github.com>
2021-02-09 15:58:08 +0100

    Update reactionrole.js

--- commands/reactionrole.js
+++ commands/reactionrole.js
@@ -41,11 +41,54 @@ module.exports = {
     const dpsRole = message.guild.roles.cache.has(dpsID);
     
     let embed = new Discord.MessageEmbed()
-      .setColor('#004a93')
+      .setColor('#004A94')
       .setTitle('Chose the class of your main character.')
-      .setDescription('Chosing a class is a must to get **Member**-status later.\n\n'
-        + 
+      .setDescription("Choosing your class is a must to get **Member** status later.\n<:druid:808301207206494228> for druid\n<:hunter:808302350162591784> for hunter\n<:warrior:808301549712572426> for warrior\n<:warlock:808301265388830730> for warlock\n<:paladin:808301574761742346> for paladin\n<:priest:808301254677495848> for priest\n<:rogue:808302389223489557> for rogue\n<:mage:808301245877059604> for mage")
+    
+    let messageEmbed = await message.channel.send(embed);
+    messageEmbed.react(druidEmoji);
+    messageEmbed.react(hunterEmoji);
+    messageEmbed.react(warriorEmoji);
+    messageEmbed.react(warlockEmoji);
+    messageEmbed.react(paladinEmoji);
+    messageEmbed.react(priestEmoji);
+    messageEmbed.react(rogueEmoji);
+    messageEmbed.react(mageEmoji);
+    
+    bot.on('messageReactionAdd', async (reaction, user) => {
+      if (reaction.message.partial) await reaction.message.fetch();
+      if (reaction.partial) await reaction.fetch();
+      if (user.bot) return;
+      ir (!reaction.message.guild) return;
+      
+      if (reaction.message.channel.id == channel) {
+        if (reaction.emoji.name === druidEmoji) {
+          await reaction.message.guild.members.cache.get(user.id).roles.add(druidID);
+        }
+        if (reaction.emoji.name === hunterEmoji) {
+          await reaction.message.guild.members.cache.get(user.id).roles.add(hunterID);
+        }
+        if (reaction.emoji.name === warriorEmoji) {
+          await reaction.message.guild.members.cache.get(user.id).roles.add(warriorID);
+        }
+        if (reaction.emoji.name === warlockEmoji) {
+          await reaction.message.guild.members.cache.get(user.id).roles.add(warlockID);
+        }
+        if (reaction.emoji.name === paladinEmoji) {
+          await reaction.message.guild.members.cache.get(user.id).roles.add(paladinID);
+        }
+        if (reaction.emoji.name === priestEmoji) {
+          await reaction.message.guild.members.cache.get(user.id).roles.add(priestID);
+        }
+        if (reaction.emoji.name === rogueEmoji) {
+          await reaction.message.guild.members.cache.get(user.id).roles.add(rogueID);
+        }
+        if (reaction.emoji.name === mageEmoji) {
+          await reaction.message.guild.members.cache.get(user.id).roles.add(mageID);
+        }
+      } else {
+        return;
+      }
+    });
   }
-
-
 }
