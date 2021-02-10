f6182009f76aa4af8dbed3d178bd76f9f436b3cf
Pirupuku <55432521+Pirupuku@users.noreply.github.com>
2021-02-10 19:46:07 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'ping',
     description: "this is a ping command!",
     execute(message, args, nickname){
-        message.channel.send('pong!');
+        message.channel.send(`Here is your pong, ${nickname}!`);
         console.log(message.author.username);
         console.log(nickname);
     }
