ecf18f59b8e1055d225fdb1f1951acad3258021a
Pirupuku <55432521+Pirupuku@users.noreply.github.com>
2021-02-10 17:30:22 +0100

    Update main.js

--- main.js
+++ main.js
@@ -45,8 +45,8 @@ var version = '1.0.0';
 
 bot.on('ready', () => {
      console.log('Many Whelps hands out 50 DKP minus');
+     bot.user.setAvatar(fs.readFileSync('./image/many_whelps_final.png'));
      bot.user.setActivity('$help', { type: 'WATCHING'}).catch(console.error);
-     bot.user.setAvatar('https://cdn.discordapp.com/attachments/801916760482644008/808741524204290058/many_whelps_final.png');
      manyWhelps(bot);
 });
 
