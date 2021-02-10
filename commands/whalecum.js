706e3de32d49a65429b6719ea4a8d2d67671aea2
Pirupuku <55432521+Pirupuku@users.noreply.github.com>
2021-02-10 23:42:49 +0100

    A

--- commands/whalecum.js
+++ commands/whalecum.js
@@ -4,6 +4,6 @@ module.exports = {
     name: 'whalecum',
     description: "a lot of whalecum!",
     execute(message, args, nickname){
-        message.channel.send('BIG\n' + 'https://i.redd.it/u1uz1jkyshjy.jpg\n' + `from ${nickname}`);
+        message.channel.send(`BIG whalecum from ${nickname}\n` + 'https://i.redd.it/u1uz1jkyshjy.jpg\n');
     }
 }
