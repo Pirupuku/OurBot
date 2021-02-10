0349f15d066544790fb68056d9f5444bc29863a2
Pirupuku <55432521+Pirupuku@users.noreply.github.com>
2021-02-10 11:52:37 +0100

    Update newmember.js

--- commands/newmember.js
+++ commands/newmember.js
@@ -26,7 +26,7 @@ module.exports = {
                 }
             ]
         };
-        message.channel.send({ embedTrialGuest });
+        //message.channel.send({ embedTrialGuest });
         message.channel.send({ embedClassRole });
     }
 }
