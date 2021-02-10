4cb20543ddac2f96cdd06cff2975982a256c0e3a
Pirupuku <55432521+Pirupuku@users.noreply.github.com>
2021-02-10 14:53:51 +0100

    Update whalecum.js

--- commands/whalecum.js
+++ commands/whalecum.js
@@ -1,7 +1,10 @@
+const { MessageAttachment } = require("discord.js")
+
 module.exports = {
     name: 'whalecum',
     description: "a lot of whalecum!",
     execute(message, args){
-        message.channel.send('https://www.google.com/search?q=whalecum&rlz=1C1CHBF_deDE893DE893&sxsrf=ALeKk00cwJCUcmWZ5wgNk5J2aNgpQlePPw:1612388237867&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiWuKGh1s7uAhWNqaQKHQD6B88Q_AUoAXoECBkQAw&biw=1920&bih=937#imgrc=J8GEqPyy2mGEuM')
+        const attachment = new MessageAttachment ('https://www.google.com/search?q=whalecum&rlz=1C1CHBF_deDE893DE893&sxsrf=ALeKk00cwJCUcmWZ5wgNk5J2aNgpQlePPw:1612388237867&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiWuKGh1s7uAhWNqaQKHQD6B88Q_AUoAXoECBkQAw&biw=1920&bih=937#imgrc=J8GEqPyy2mGEuM');
+        message.channel.send(attachment);
     }
 }
