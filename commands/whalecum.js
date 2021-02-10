9aac109b244ffcc2a05ef48eca5792ca66015e0c
Pirupuku <55432521+Pirupuku@users.noreply.github.com>
2021-02-10 15:09:46 +0100

    Update whalecum.js

--- commands/whalecum.js
+++ commands/whalecum.js
@@ -4,7 +4,7 @@ module.exports = {
     name: 'whalecum',
     description: "a lot of whalecum!",
     execute(message, args){
-        const attachment = new MessageAttachment ('https://www.google.com/search?q=whalecum&rlz=1C1CHBF_deDE893DE893&sxsrf=ALeKk00cwJCUcmWZ5wgNk5J2aNgpQlePPw:1612388237867&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiWuKGh1s7uAhWNqaQKHQD6B88Q_AUoAXoECBkQAw&biw=1920&bih=937#imgrc=J8GEqPyy2mGEuM');
-        message.channel.send(attachment);
+        //const attachment = new MessageAttachment ('https://i.redd.it/u1uz1jkyshjy.jpg');
+        message.channel.send('https://i.redd.it/u1uz1jkyshjy.jpg');
     }
 }
