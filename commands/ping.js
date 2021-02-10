7c3117cbdd40a9c285379d2cac0036710d48d12c
Pirupuku <55432521+Pirupuku@users.noreply.github.com>
2021-02-10 19:10:05 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -3,6 +3,7 @@ module.exports = {
     description: "this is a ping command!",
     execute(message, args){
         console.log(message.author.username);
+        console.log(message.author.displayName);
         message.channel.send('pong!');
     }
 }
