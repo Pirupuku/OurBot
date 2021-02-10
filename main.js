00d6a449ecb864d119a00157f0593fe83641406b
Pirupuku <55432521+Pirupuku@users.noreply.github.com>
2021-02-10 12:44:46 +0100

    Update main.js

--- main.js
+++ main.js
@@ -57,17 +57,17 @@ bot.on('message', message =>{
       if (!message.content.startsWith(prefix) && !message.author.bot) {
          message.delete({ timeout: 10000 });
       }
+      if (command == 'trial') {
+         bot.commands.get('trial').execute(message, args);
+      } else if (command == 'guest') {
+         bot.commands.get('guest').execute(message, args);
+      }
       if (message.member.hasPermission('MANAGE_MESSAGES')) {
          if (command == 'newmember') {
             bot.commands.get('newmember').execute(Discord, message, args);
          } 
       } else {
          return message.author.send('You don not have the permission to do that.');
-      }
-      if (command == 'trial') {
-         bot.commands.get('trial').execute(message, args);
-      } else if (command == 'guest') {
-         bot.commands.get('guest').execute(message, args);
       }
    }
    if (!message.content.startsWith(prefix) || message.author.bot) return;
