ea5e136c8db6f8b2e30be1d3af7aa721ec683dee
Pirupuku <55432521+Pirupuku@users.noreply.github.com>
2021-02-10 19:19:47 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'ping',
     description: "this is a ping command!",
     execute(bot, message, args){
-        let guild = bot.guilds.get('773542499049668608');
+        let guild = bot.guilds.fetch('773542499049668608');
         let member = guild.member(message.author);
         let nickname = member ? member.displayName : null;
         
