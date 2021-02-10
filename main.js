262828d3c6285bfd33b71265b7508b2206e445e5
Pirupuku <55432521+Pirupuku@users.noreply.github.com>
2021-02-10 17:38:24 +0100

    Update main.js

--- main.js
+++ main.js
@@ -45,7 +45,6 @@ var version = '1.0.0';
 
 bot.on('ready', () => {
      console.log('Many Whelps hands out 50 DKP minus');
-     bot.user.setAvatar(fs.readFileSync('./image/many_whelps_final.png'));
      bot.user.setActivity('$help', { type: 'WATCHING'}).catch(console.error);
      manyWhelps(bot);
 });
