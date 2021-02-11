952cd77cbfa6e7e47eb4fe167c88d14035f67301
Pirupuku <55432521+Pirupuku@users.noreply.github.com>
2021-02-11 18:14:25 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -3,6 +3,6 @@ module.exports = {
     description: "this is a ping command!",
     execute(message, args, nickname){
         message.channel.send(`Here is your pong, ${nickname}!`);
-        console.log(message.guild.roles.get('798497871376547881').members.map(m=>m.user.tag));
+        console.log(message.guild.roles.cache.get('798497871376547881').members.map(m=>m.user.tag));
     }
 }
