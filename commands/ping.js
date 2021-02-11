3c16804810c203e87c4cbedf076834d4c3f5d8a3
Pirupuku <55432521+Pirupuku@users.noreply.github.com>
2021-02-11 18:16:32 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -3,6 +3,13 @@ module.exports = {
     description: "this is a ping command!",
     execute(message, args, nickname){
         message.channel.send(`Here is your pong, ${nickname}!`);
-        console.log(message.guild.roles.cache.get('798497871376547881').members.map(m=>m.user.tag));
+        console.log(message.guild.roles.cache.get('798497796541513759').members.map(m=>m.user.tag));
+        console.log(message.guild.roles.cache.get('798497755952578560').members.map(m=>m.user.tag));
+        console.log(message.guild.roles.cache.get('798497704976056350').members.map(m=>m.user.tag));
+        console.log(message.guild.roles.cache.get('799233980570402816').members.map(m=>m.user.tag));
+        console.log(message.guild.roles.cache.get('798497653058830339').members.map(m=>m.user.tag));
+        console.log(message.guild.roles.cache.get('798497604035805184').members.map(m=>m.user.tag));
+        console.log(message.guild.roles.cache.get('798497338561789993').members.map(m=>m.user.tag));
+        console.log(message.guild.roles.cache.get('784698785586348032').members.map(m=>m.user.tag));
     }
 }
