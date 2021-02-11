f2328ae8e47e3db542c823ccf3c50f4e45e355c6
Pirupuku <55432521+Pirupuku@users.noreply.github.com>
2021-02-11 06:57:53 +0100

    Update newmember.js

--- commands/newmember.js
+++ commands/newmember.js
@@ -2,29 +2,27 @@ module.exports = {
     name: 'newmember',
     description: "embed for new members",
     execute(Discord, message, args){
-        const embedTrialGuest = new Discord.MessageEmbed()
+        const embedNewmember = new Discord.MessageEmbed()
+            .setAuthor('ManyWhelps', 'https://cdn.discordapp.com/attachments/801916760482644008/808741524204290058/many_whelps_final.png')
+            .setDescription('To get the status of a **member** later on, doing as written below is a necessary step.')
             .setColor('#004A94')
             .addFields (
                 {
-                    name: 'Do you want to join the guild and get **trial** status OR were you invited as a friend/for a PuG?',
+                    name: '**1.** Do you want to join the guild and get **trial** status OR were you invited as a friend/for a PuG?',
                     value: '```Please write either $trial OR $guest```',
                     inline: false
                 },
+                {
+                    name: '**2.** If you choosed TRIAL -> write the Class of your main character',
+                    value: '```Please write $class {yourclass} so you get the role of your class.\n\nEg: $class druid -> you get the discord role of a druid```',
+                    inline: false
+                },
+                {
+                    name: '**3.** Role of your main character in raids',
+                    value: '```Please write $role {yourrole} so that the bot gives you the specific role, which you want to have in raids.\n\nEg: $role healer -> you get the discord role of a healer```',
+                    inline: false
+                },
             );
-        const embedClassRole = new Discord.MessageEmbed()
-            .setDescription('To get the status of a **member** later on, doing as written below is a necessary step.')
-            .setColor('#004A94')
-            .addField (
-                    'Class of the main character',
-                    '```Please write $class {yourclass} so you get the role of your class.\n\nEg: $class druid -> you get the discord role of a druid```',
-                    false
-            )
-            .addField (
-                    'Role of the main character',
-                    '```Please write $role {yourrole} so that the bot gives you the specific role, which you want to have in raids.\n\nEg: $role healer -> you get the discord role of a healer```',
-                    false
-            )
-        message.channel.send( embedTrialGuest );
-        message.channel.send( embedClassRole );
+        message.channel.send( embedNewmember );
     }
 }
