ab9c891113e67801c3c3827704ffc8d8e0aa3eb3
Pirupuku <55432521+Pirupuku@users.noreply.github.com>
2021-02-11 18:32:11 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -4,12 +4,15 @@ module.exports = {
     execute(message, args, nickname){
         message.channel.send(`Here is your pong, ${nickname}!`);
         console.log(message.guild.roles.cache.get('798497796541513759').members.displayName);
-        console.log(message.guild.roles.cache.get('798497755952578560').members.map(m=>m.user.tag));
+        console.log(message.guild.roles.cache.get('798497755952578560').members.map(m=>m.user.id));
         console.log(message.guild.roles.cache.get('798497704976056350').members.map(m=>m.user.tag));
         console.log(message.guild.roles.cache.get('799233980570402816').members.map(m=>m.user.tag));
         console.log(message.guild.roles.cache.get('798497653058830339').members.map(m=>m.user.tag));
         console.log(message.guild.roles.cache.get('798497604035805184').members.map(m=>m.user.tag));
         console.log(message.guild.roles.cache.get('798497338561789993').members.map(m=>m.user.tag));
         console.log(message.guild.roles.cache.get('784698785586348032').members.map(m=>m.user.tag));
+        let guild = bot.guilds.cache.get('773542499049668608');
+        let member = guild.member(message.author);
+        let nickname = member ? member.displayName : null;
     }
 }
