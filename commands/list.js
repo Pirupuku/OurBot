3db59bbb316a70d3e8b66d3786f7c4335ea29294
Pirupuku <55432521+Pirupuku@users.noreply.github.com>
2021-02-12 07:18:24 +0100

    Update list.js

--- commands/list.js
+++ commands/list.js
@@ -17,7 +17,7 @@ module.exports = {
         var test = '';
 
         for (var i = 0; i < druidName.length; i++) {
-            test = args[i] & ',' & ' ' & test;
+            test = args[i] + ',' + ' ' + test;
         }
         console.log(test);
 
