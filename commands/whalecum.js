104a374bba8936db6d021b71985122639f0e19a4
Pirupuku <55432521+Pirupuku@users.noreply.github.com>
2021-02-15 19:35:48 +0100

    Update whalecum.js

--- commands/whalecum.js
+++ commands/whalecum.js
@@ -3,7 +3,7 @@ const { MessageAttachment } = require("discord.js")
 module.exports = {
     name: 'whalecum',
     description: "a lot of whalecum!",
-    execute(message, args, nickname){
+    execute(bot, message, args, nickname){
         message.channel.send(`BIG whalecum from ${nickname}\n` + 'https://i.redd.it/u1uz1jkyshjy.jpg\n');
     }
 }
