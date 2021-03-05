72618ebbe96fd44bafe3b712f94c56a13ab4f706
Pirupuku <55432521+Pirupuku@users.noreply.github.com>
2021-03-05 15:59:12 +0100

    Update member-counter.js

--- counters/member-counter.js
+++ counters/member-counter.js
@@ -1,13 +1,13 @@
 module.exports = async(bot, message) =>{
     const channelId = '805473191177748491';
 
-    const updateMembers = guild => {
+    const updateMembers = (guild) => {
         const channel = guild.channels.cache.get(channelId);
-        channel.setName(`Total Members: ${guild.manyWhelps}`);
+        channel.setName(`Members: ${guild.manyWhelps}`);
     }
     
-    bot.on('guildMemberAdd', member => updateMembers(member.guild))
-    bot.on('guildMemberRemove', member => updateMembers(member.guild))
+    bot.on('guildMemberAdd', (member) => updateMembers(member.guild))
+    bot.on('guildMemberRemove', (member) => updateMembers(member.guild))
     
     const guild = bot.guilds.cache.get('773542499049668608')
     updateMembers(guild)
