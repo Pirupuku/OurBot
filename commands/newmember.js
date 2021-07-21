e5eedbdcf7c0445f4895afa3c4bfef60fe67b96b
Pirupuku <55432521+Pirupuku@users.noreply.github.com>
2021-07-21 10:09:53 +0200

    Update newmember.js

--- commands/newmember.js
+++ commands/newmember.js
@@ -4,8 +4,9 @@ module.exports = {
     execute(Discord, bot, message, args){
         const embedNewmember = new Discord.MessageEmbed()
             .setAuthor('[A][LC][EU] <Many Whelps>', 'https://cdn.discordapp.com/attachments/801916760482644008/808741524204290058/many_whelps_final.png')
-            .setDescription("Hey there! If you are here to join the guild as a TRIAL than DM one of the officers @Ajso , @Divi or @Dieken.\nIf you are here as a PuG for a raid, to participate in PVP/Premades or as friend, than type $guest into the chat.")
+            //.setDescription("Hey there! If you are here to join the guild as a TRIAL than DM one of the officers @Ajso , @Divi or @Dieken.\nIf you are here as a PuG for a raid, to participate in PVP/Premades or as friend, than type $guest into the chat.")
             .setColor('#004A94')
+            .addField('Currently under construction')
         bot.channels.cache.get('808820077561774100').send(embedNewmember);
     }
 }
