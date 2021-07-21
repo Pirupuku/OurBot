fc04d5a99b3782eb8a167302c86ee2398f2383ca
Pirupuku <55432521+Pirupuku@users.noreply.github.com>
2021-07-21 10:27:00 +0200

    fixxing

--- commands/class_embed.js
+++ commands/class_embed.js
@@ -5,9 +5,9 @@ module.exports = {
     description: 'makes a class embed',
     execute(Discord, message) {
         var ClassEmbed = new Discord.MessageEmbed()
-            .setTitle("Choose your class");
-            .setDescription('Click on the symbol, so we know which class you MAIN is! You can chose just ONE class, even if you click more symbols you just get the class of the latest icon you clicked.');
-            .setColor('#004A94');
+            .setTitle("Choose your class")
+            .setDescription('Click on the symbol, so we know which class you MAIN is! You can chose just ONE class, even if you click more symbols you just get the class of the latest icon you clicked.')
+            .setColor('#004A94')
         message.channel.send({embed: ClassEmbed})
         .then(embedMessage => {
             for (key in ClassEmoji)
--- commands/role_embed.js
+++ commands/role_embed.js
@@ -5,9 +5,9 @@ module.exports = {
     description: 'makes a role embed',
     execute(Discord, message) {
         var RoleEmbed = new Discord.MessageEmbed()
-            .setTitle("Choose your role");
-            .setDescription('Click on the symbol, so we know which role you want to play in RAIDS! You can chose just ONE role, even if you click more symbols you just get the role of the latest icon you clicked.');
-            .setColor('#004A94');
+            .setTitle("Choose your role")
+            .setDescription('Click on the symbol, so we know which role you want to play in RAIDS! You can chose just ONE role, even if you click more symbols you just get the role of the latest icon you clicked.')
+            .setColor('#004A94')
         message.channel.send({embed: RoleEmbed})
         .then(embedMessage => {
             for (key in RoleEmoji)
