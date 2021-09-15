735795a35ff0f83347a7764855f88d37b917eedc
Pirupuku <55432521+Pirupuku@users.noreply.github.com>
2021-09-15 11:08:57 +0200

    Update member-counter.js

--- counters/member-counter.js
+++ counters/member-counter.js
@@ -2,25 +2,27 @@ module.exports = (bot) => {
     const channelId = '805473191177748491';
     
     var updateMembers = (guild) => {
-        const botRole = bot.guilds.cache.get('773542499049668608').roles.cache.find(role => role == '798524203120197652');
-        const guestRole = bot.guilds.cache.get('773542499049668608').roles.cache.find(role => role == '808291239924465694');
-        const trialRole = bot.guilds.cache.get('773542499049668608').roles.cache.find(role => role == '798498129661263882');
-        const memberRole = bot.guilds.cache.get('773542499049668608').roles.cache.find(role => role == '773548993040482346');
-        const socialRole = bot.guilds.cache.get('773542499049668608').roles.cache.find(role => role == '798512429175668736');
-        const raiderRole = bot.guilds.cache.get('773542499049668608').roles.cache.find(role => role == '798498626442887199');
-        const officerRole = bot.guilds.cache.get('773542499049668608').roles.cache.find(role => role == '798497871376547881');
-        const bots = bot.guilds.cache.get('773542499049668608').members.cache.filter(member => member.roles.cache.find(role => role == botRole)).map(member => member.user.tag).length
-        const guests = bot.guilds.cache.get('773542499049668608').members.cache.filter(member => member.roles.cache.find(role => role == guestRole)).map(member => member.user.tag).length
-        const trial = bot.guilds.cache.get('773542499049668608').members.cache.filter(member => member.roles.cache.find(role => role == trialRole)).map(member => member.user.tag).length
-        const member = bot.guilds.cache.get('773542499049668608').members.cache.filter(member => member.roles.cache.find(role => role == memberRole)).map(member => member.user.tag).length
-        const social = bot.guilds.cache.get('773542499049668608').members.cache.filter(member => member.roles.cache.find(role => role == socialRole)).map(member => member.user.tag).length
-        const raider = bot.guilds.cache.get('773542499049668608').members.cache.filter(member => member.roles.cache.find(role => role == raiderRole)).map(member => member.user.tag).length
-        const officer = bot.guilds.cache.get('773542499049668608').members.cache.filter(member => member.roles.cache.find(role => role == officerRole)).map(member => member.user.tag).length
-        const withoutRole = guests + trial + member + social + raider + officer + bots
-        const channel = guild.channels.cache.get(channelId);
-        var realNumber = guild.memberCount - bots - guests - (guild.memberCount - withoutRole);
-        channel.setName(`Whelps: ${realNumber}`);
-        console.log(realNumber)
+        if guild.channels.exists('name', corruption) {
+            const botRole = bot.guilds.cache.get('773542499049668608').roles.cache.find(role => role == '798524203120197652');
+            const guestRole = bot.guilds.cache.get('773542499049668608').roles.cache.find(role => role == '808291239924465694');
+            const trialRole = bot.guilds.cache.get('773542499049668608').roles.cache.find(role => role == '798498129661263882');
+            const memberRole = bot.guilds.cache.get('773542499049668608').roles.cache.find(role => role == '773548993040482346');
+            const socialRole = bot.guilds.cache.get('773542499049668608').roles.cache.find(role => role == '798512429175668736');
+            const raiderRole = bot.guilds.cache.get('773542499049668608').roles.cache.find(role => role == '798498626442887199');
+            const officerRole = bot.guilds.cache.get('773542499049668608').roles.cache.find(role => role == '798497871376547881');
+            const bots = bot.guilds.cache.get('773542499049668608').members.cache.filter(member => member.roles.cache.find(role => role == botRole)).map(member => member.user.tag).length
+            const guests = bot.guilds.cache.get('773542499049668608').members.cache.filter(member => member.roles.cache.find(role => role == guestRole)).map(member => member.user.tag).length
+            const trial = bot.guilds.cache.get('773542499049668608').members.cache.filter(member => member.roles.cache.find(role => role == trialRole)).map(member => member.user.tag).length
+            const member = bot.guilds.cache.get('773542499049668608').members.cache.filter(member => member.roles.cache.find(role => role == memberRole)).map(member => member.user.tag).length
+            const social = bot.guilds.cache.get('773542499049668608').members.cache.filter(member => member.roles.cache.find(role => role == socialRole)).map(member => member.user.tag).length
+            const raider = bot.guilds.cache.get('773542499049668608').members.cache.filter(member => member.roles.cache.find(role => role == raiderRole)).map(member => member.user.tag).length
+            const officer = bot.guilds.cache.get('773542499049668608').members.cache.filter(member => member.roles.cache.find(role => role == officerRole)).map(member => member.user.tag).length
+            const withoutRole = guests + trial + member + social + raider + officer + bots
+            const channel = guild.channels.cache.get(channelId);
+            var realNumber = guild.memberCount - bots - guests - (guild.memberCount - withoutRole);
+            channel.setName(`Whelps: ${realNumber}`);
+            console.log(realNumber)
+        }
     }
     
     bot.on('guildMemberAdd', (member) => updateMembers(member.guild))
