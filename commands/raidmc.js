da7364739a4689d387a3dc6b30859cf28e6fca65
Pirupuku <55432521+Pirupuku@users.noreply.github.com>
2021-10-04 15:15:36 +0200

    raid automatization 8

--- commands/raidmc.js
+++ commands/raidmc.js
@@ -3,13 +3,13 @@ module.exports = {
     description: "start raiding MC",
     execute(bot, message, args) {
 
-        let raidMC = new cron.CronJob(`00 00 20 * * 0`, () => {
+        let startMC = new cron.CronJob(`00 00 20 * * 0`, () => {
             var mcDate = new Date();
             mcDate.setDate(mcDate.getDate() + (7 + args[0] - mcDate.getDay() % 7));
             let channel = bot.guilds.cache.get('773542499049668608').channels.cache.get('800716895417532526'); // #corruption
             channel.send(`+quickcreate [title: MOLTEN CORE][description: killing shit and getting lootz!!!][channel:sign-ups][date:${mcDate.getDate()}-${mcDate.getMonth()+1}-${mcDate.getFullYear()}][time:20:00][template:2][image: https://i.ibb.co/CBVZw8h/molten-core.jpg] [advanced:# Deadline - ( 0 ) # Sign-Ups overall - ( 100 ) # Sign-Ups lock - ( false ) # Sign-Ups user - ( 1 )  # Mention - ( {Raiders} )  # Attendance - ( true ) # Numbering - ( true ) # Rename - ( Late ) # ( Bench ) # ( Tentative ) # ( Absence ) # ( No )  # Announcement - ( true ) # ( general ) # ( 1 ) # ( Raid has been posted ) # Opt-out - ( none )][deletion:4]`);
         });
-        raidMC.start();
+        startMC.start();
         message.channel.send('You will receive a message to quickly create your **Molten Core** in the channel #corruption.');
     }
 }
--- commands/raidony.js
+++ commands/raidony.js
@@ -5,14 +5,14 @@ module.exports = {
 
         var onyStartDate = new Date();
 
-        let raidMC = new cron.CronJob(`00 00 20 * * ${onyStartDate.getDay()}`, () => {
+        let startOny = new cron.CronJob(`00 00 20 * * ${onyStartDate.getDay()}`, () => {
             onyStartDate.setDate(onyStartDate.getDate() + 5);
             var onyDate = new Date();
             onyDate.setDate(onyDate.getDate() + 5);
             let channel = bot.guilds.cache.get('773542499049668608').channels.cache.get('800716895417532526'); // #corruption
             channel.send(`+quickcreate [title: ONYXIA][description: killing shit and getting lootz!!!][channel:sign-ups][date:${onyDate.getDate()}-${onyDate.getMonth()+1}-${onyDate.getFullYear()}][time:20:00][template:2][image: https://ibb.co/tYnppNJ] [advanced:# Deadline - ( 0 ) # Sign-Ups overall - ( 100 ) # Sign-Ups lock - ( false ) # Sign-Ups user - ( 1 )  # Mention - ( {Raiders} )  # Attendance - ( true ) # Numbering - ( true ) # Rename - ( Late ) # ( Bench ) # ( Tentative ) # ( Absence ) # ( No )  # Announcement - ( true ) # ( general ) # ( 1 ) # ( Raid has been posted ) # Opt-out - ( none )][deletion:4]`);
         });
-        raidMC.start();
+        startOny.start();
         message.channel.send('You will receive a message to quickly create your **Onyxia** in the channel #corruption.');
     }
 }
