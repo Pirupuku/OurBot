265c2d93e2e5e2e7f33fde363f5b06116bcc9835
Pirupuku <55432521+Pirupuku@users.noreply.github.com>
2022-06-14 06:38:57 +0200

    Update cheer.js

--- commands/cheer.js
+++ commands/cheer.js
@@ -6,7 +6,8 @@ module.exports = {
     .setDescription("Get cheered"),
   async execute(interaction, nickname, client) {
     interaction.reply({
-      content: "You won't ever be able to kill C'thun - you little piece of shit!"
+      content: "You won't ever be able to kill C'thun - you little piece of shit!",
+      ephemeral: true,
     });
   }
 }
