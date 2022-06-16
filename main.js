84281bb6b380bd810fb4ceea171523754dbd06cd
Pirupuku <55432521+Pirupuku@users.noreply.github.com>
2022-06-16 23:51:36 +0200

    Update main.js

--- main.js
+++ main.js
@@ -295,7 +295,7 @@ client.on("interactionCreate", async interaction => {
           )    
       }
 
-      await interaction.reply({ bossEmbed })
+      await interaction.reply({ embeds: [bossEmbed] })
     }
   }
 });
